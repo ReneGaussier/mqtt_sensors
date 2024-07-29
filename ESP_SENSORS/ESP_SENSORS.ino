2b1f6b50001aefe59d4771a707e8748044bef009
Rémi <135984301+ReneGaussier@users.noreply.github.com>
2024-07-29 15:47:45 +0200

    Add new features, Bettery monitor, LED status indication
    Battery Monitoring:
    
        Monitors the battery level and sends it to the MQTT broker on the dedicated topic maintopic/bat/.
        The game can display the battery level and warn the user when the battery is running low.
    
    User Interface Enhancements:
    
        An LED connected to pin 21 lights up when the device has booted and communication with the BNO055 is established.
        An LED connected to pin 23 lights up when the ESP32 is connected to Wi-Fi.
    
    Code Maintenance:
    
        Added comments to the code to ease maintenance.

--- ESP_SENSORS/ESP_SENSORS.ino
+++ ESP_SENSORS/ESP_SENSORS.ino
@@ -1,50 +1,118 @@
-#include <string.h>
-#include <stdbool.h> 
-#include <WiFi.h>
-#include <PubSubClient.h>
-#include <Adafruit_Sensor.h>                                                    
-#include <Adafruit_BNO055.h>   
+/*
+* This file is a part of Mqtt_Sensors
+*
+* Copyright (C) 2023 Inria
+*
+* This software is governed by the CeCILL license under French law and
+* abiding by the rules of distribution of free software.  You can  use, 
+* modify and/ or redistribute the software under the terms of the CeCILL
+* license as circulated by CEA, CNRS and INRIA at the following URL
+* "http://www.cecill.info". 
+*
+* As a counterpart to the access to the source code and  rights to copy,
+* modify and redistribute granted by the license, users are provided only
+* with a limited warranty  and the software's author,  the holder of the
+* economic rights,  and the successive licensors  have only  limited
+* liability.
+*/
 
-WiFiClient espClient;
-PubSubClient client(espClient);
+// MQTT Broker
+#include "TinyMqtt.h"
+
+#include <string.h>
+#include <stdbool.h>
+//#include <ESP32>
+#include <WiFi.h>
+//#include <ESP8266WiFi.h> // lib for the ESP 8266, not compatible with the ESP32
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+
+#define PORT 1883
+
+#define CLOSE 0
+#define OPEN 1
+#define FLEX_LOW_LIMIT 1300
+#define FLEX_HIGH_LIMIT 1400
 
 /* ESP IMU configuration */
 struct esp_config {
-    char* ssid;
-    char* password;
-    char* mqtt_server;
-    /* main and suffix used for topic */
-    char* topic_main;
-    char* topic_suffix;
-    /* delay in MS between two samples*/
-    int delay;
-    /* Gyro Z */
-    bool rot_speed;
-    /* Orientation Y */
-    bool tilt;
-    /* Euler orientation */
-    bool orientation;
-    /* Quaternion orientation */
-    bool quaternion;
-    /* EMG sensor */
-    bool emg;
-    /* Buttons */
-    bool buttons;
-    /* Display data on serial line */
-    bool display;
+  char* ssid;
+  char* password;
+  char* mqtt_server;
+  /* main and suffix used for topic */
+  char* topic_main;
+  char* topic_suffix;
+  /* delay in MS between two samples*/
+  int delay;
+  /* Gyro Z */
+  bool rot_speed;
+  /* Orientation Y */
+  bool tilt;
+  /* Euler orientation */
+  bool orientation;
+  /* Quaternion orientation */
+  bool quaternion;
+  /* EMG sensor */
+  bool emg;
+  /* Buttons */
+  bool flex;
+  /* Display data on serial line */
+  bool display;
+  /* Hosts Wi-Fi hotspot and MQTT broker */
+  bool master;
+  /* Enable statu LED */
+  bool status_led;
+  /* Status LED PIN */
+  int status_led_pin;
+  /* Enable Wifi Status LED*/
+  bool wifi_led;
+  /* Wifi LED PIN*/
+  int led_wifi_pin;
+  /* Battery monitor */
+  bool battery_monitor_enable;
+  /* anlog pin to read the battery value */
+  int battery_voltage_pin;
+  /* max voltage battery */
+  float max_battery_voltage;
 };
 
-//struct esp_config cfg = {"fit_and_fun", "", "10.42.0.1","fit_and_fun", "", 100, true, false, false, false, false, false, true};
-struct esp_config cfg = {"honor5_roger", "", "192.168.43.78", "fit_and_fun", "_1", 100, false, false, true, true, false, false, true};
+
+
+// struct esp_config cfg = {"fit_and_fun","", "10.42.0.1","fit_and_fun","",100, true,false,false,false, false, false, true, false, true,15, true, 26, 4.2};
+struct esp_config cfg = {
+  "Your_Wifi_AP",                                    // SSID for the Wi-Fi network
+  "Your_passphrase",   				     // Password for the Wi-Fi network (empty)
+  "192.168.1.69",                                    // Address of the MQTT server
+  "fit_and_fun",                                     // Main topic for MQTT communication
+  "",                                                // Suffix for the topic (empty)
+  100,                                               // Delay between samples in milliseconds
+  true,                                              // Enable Gyro Z
+  false,                                             // Enable Orientation Y
+  false,                                             // Enable Euler orientation
+  false,                                             // Enable Quaternion orientation
+  false,                                             // Enable EMG sensor
+  false,                                             // Enable Buttons (flex sensor)
+  true,                                              // Display data on serial line
+  false,                                             // Hosts Wi-Fi hotspot and MQTT broker
+  true,                                              // Enable Status LED
+  15,                                                // Pin for the status LED PIN
+  true,                                              // Enable Wi-Fi status LED
+  27,                                                // Pin for Wi-Fi LED
+  true,                                              // Enable battery monitor
+  34,                                                // Analog pin to read battery voltage . A2 on feather => pin 34
+  4.2                                                // Maximum battery voltage (usualy 4.2v for Li-po)
+};
+
+
+MqttBroker broker(PORT);
+MqttClient client(&broker);
 
 /* Hardware pin for definition and leds */
-const int buttonOnePin = 14;
-const int buttonTwoPin = 15;
-const int ledPin =  13; 
 const int emgPin = 36;
+const int flexPin = 33;
 
-/* Check I2C device address */                                                          
-Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28); 
+/* Check I2C device address */
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 /* Mqtt string messages */
 #define MSG_BUFFER_SINGLE_SIZE (10)
 #define MSG_BUFFER_VECTOR3_SIZE (30)
@@ -59,62 +127,101 @@ char topic_quaternion[TOPIC_SIZE];
 char topic_tilt[TOPIC_SIZE];
 char topic_rot_speed[TOPIC_SIZE];
 char topic_emg[TOPIC_SIZE];
-char topic_button1[TOPIC_SIZE];
-char topic_button2[TOPIC_SIZE]; 
+char topic_battery[TOPIC_SIZE];
 /* Sensor variables initialisation */
-int buttonOneState = 0;
-int buttonTwoState = 0;
 float emg = 0.0F;
+int flexValue = 0;
+bool flexState = false;
 
 /* Network connection */
 void setupWifi();
 void reConnect();
+float batteryLevel();
+
+//void disableTaskWDT() {
+//  esp_task_wdt_init(30, false); // Set timeout to 30 seconds (sufficient for most debugging)
+//  esp_task_wdt_delete(NULL);    // Remove watchdog from current task
+//}
 
 /* Arduino code initialisation (run once) */
 void setup() {
-  Serial.begin(115200); 
-  setupWifi(); 
-  /* Mqtt init */
-  client.setServer(cfg.mqtt_server, 1883); 
-  
+  Serial.begin(115200);
+  // Define the Wifi LED indicator
+  if (cfg.wifi_led) {
+    pinMode(cfg.led_wifi_pin, OUTPUT);
+    digitalWrite(cfg.led_wifi_pin, LOW);  
+  }
+
+  if (cfg.status_led) {
+    pinMode(cfg.status_led_pin, OUTPUT);
+    digitalWrite(cfg.status_led_pin, LOW); 
+  }
+  //disableTaskWDT();
+  setupWifi();
+  if (cfg.master) {
+    /* Start the broker */
+    broker.begin();
+    Serial.print("Starting MQTT broker on address : ");
+    Serial.println(WiFi.softAPIP());  // Ip of the broker
+  } else {
+    /* Connect client to specified broker adress */
+    client.connect(cfg.mqtt_server, PORT);
+  }
+  /* Initialise the sensor */
+  if (!bno.begin()) {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1)
+      ;
+  }
+
+  /* If the sensor is reconized, turn on the status LED*/
+  if (cfg.status_led) {
+    pinMode(cfg.status_led_pin, OUTPUT);
+    digitalWrite(cfg.status_led_pin, HIGH);
+  }
+
+
   /* construct topics complete name */
   snprintf(topic_orientation, TOPIC_SIZE, "%s/orientation%s", cfg.topic_main, cfg.topic_suffix);
   snprintf(topic_quaternion, TOPIC_SIZE, "%s/quaternion%s", cfg.topic_main, cfg.topic_suffix);
   snprintf(topic_tilt, TOPIC_SIZE, "%s/tilt%s", cfg.topic_main, cfg.topic_suffix);
-  snprintf(topic_rot_speed, TOPIC_SIZE, "%s/rot_speed%s", cfg.topic_main, cfg.topic_suffix);
+  snprintf(topic_rot_speed, TOPIC_SIZE, "%s/speed/%s", cfg.topic_main, cfg.topic_suffix);
   snprintf(topic_emg, TOPIC_SIZE, "%s/emg%s", cfg.topic_main, cfg.topic_suffix);
-  snprintf(topic_button1, TOPIC_SIZE, "%s/button1%s", cfg.topic_main, cfg.topic_suffix);
-  snprintf(topic_button2, TOPIC_SIZE, "%s/button2%s", cfg.topic_main, cfg.topic_suffix);
+  snprintf(topic_battery, TOPIC_SIZE, "%s/bat/%s", cfg.topic_main, cfg.topic_suffix);
 
-  /* Initialise the sensor */                                                   
-  if (!bno.begin())                                                             
-  {                                                                             
-    /* There was a problem detecting the BNO055 ... check your connections */   
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");                                                                              
-    while (1);                                                                  
-  }       
-
-  /* Initialize the two button inputs */
-  if (cfg.buttons) {
-    pinMode(buttonOnePin, INPUT_PULLUP);
-    pinMode(buttonTwoPin, INPUT_PULLUP);
-    pinMode(ledPin, OUTPUT);
-  }
-
-  delay(1000);                                                                                   
-                  
+  delay(1000);
 }
 
 /* Arduino code run continuously */
 void loop() {
-  if (!client.connected()) {
-        reConnect();
-    }
-  client.loop(); 
-  
+  float bat_level;
+
+  /* MQTT */
+  client.loop();
+  if (cfg.master) {
+    broker.loop();
+  }
+
+  if (cfg.battery_monitor_enable) {
+    // get the batterry percentage
+    float bat_level = batteryLevel(cfg.battery_voltage_pin, cfg.max_battery_voltage);
+    client.publish(topic_battery, String(bat_level));
+  }
+
+    /* blink the status LED when the battery is running low */
+  if ( (cfg.status_led) && (cfg.battery_monitor_enable)  && (bat_level < 3.5 ) ) {
+    // not implemented yet
+    //just brith the led on pin 13 monted on the board
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+
   /* Get a new sensor event */
   sensors_event_t eventOrientation, eventGyro;
-  if ((cfg.orientation) ||  (cfg.quaternion) ||  (cfg.tilt) ) {
+
+
+  if ((cfg.orientation) || (cfg.quaternion) || (cfg.tilt)) {
     /* Get a new sensor event */
     bno.getEvent(&eventOrientation, Adafruit_BNO055::VECTOR_EULER);
     imu::Quaternion quat = bno.getQuat();
@@ -132,8 +239,8 @@ void loop() {
         Serial.print(" qZ: ");
         Serial.print(quat.z(), 4);
         Serial.println("");
-      }   
-      if (cfg.quaternion) { 
+      }
+      if (cfg.orientation) {
         /* Euler */
         Serial.print("or_x:");
         Serial.print((float)eventOrientation.orientation.x);
@@ -143,7 +250,7 @@ void loop() {
         Serial.print((float)eventOrientation.orientation.z);
         Serial.println("");
       }
-       if (cfg.tilt) { 
+      if (cfg.tilt) {
         /* Tilt */
         Serial.print("tilt:");
         Serial.print((float)eventOrientation.orientation.y);
@@ -151,22 +258,25 @@ void loop() {
     }
     /* Creation and sending mqtt messages */
     if (cfg.orientation) {
-      snprintf(msg_vector3, MSG_BUFFER_VECTOR3_SIZE, "%6.2f %6.2f %6.2f", eventOrientation.orientation.x, eventOrientation.orientation.y, eventOrientation.orientation.z );
-      client.publish(topic_orientation,msg_vector3);
+      snprintf(msg_vector3, MSG_BUFFER_VECTOR3_SIZE, "%6.2f %6.2f %6.2f", eventOrientation.orientation.x, eventOrientation.orientation.y, eventOrientation.orientation.z);
+      client.publish(topic_orientation, msg_vector3);
     }
-     if (cfg.quaternion) {
-      snprintf(msg_vector4, MSG_BUFFER_VECTOR4_SIZE, "%2.6f %2.6f %2.6f %2.6f", quat.w(), quat.x(), quat.y(), quat.z() );
-      client.publish(topic_quaternion,msg_vector4);
+    if (cfg.quaternion) {
+      snprintf(msg_vector4, MSG_BUFFER_VECTOR4_SIZE, "%2.6f %2.6f %2.6f %2.6f", quat.w(), quat.x(), quat.y(), quat.z());
+      client.publish(topic_quaternion, msg_vector4);
     }
     if (cfg.tilt) {
       snprintf(msg_single, MSG_BUFFER_SINGLE_SIZE, "%6.2f", (float)eventOrientation.orientation.y);
       client.publish(topic_tilt, msg_single);
     }
   }
+
   if (cfg.rot_speed) {
     /* Get a new sensor event */
     bno.getEvent(&eventGyro, Adafruit_BNO055::VECTOR_GYROSCOPE);
-    /* Print plotable ide arduino data on serial port */ 
+    /* Print plotable ide arduino data on serial port */
+
+
     if (cfg.display) {
       Serial.print("gyr_x:");
       Serial.print((float)eventGyro.gyro.x);
@@ -179,28 +289,15 @@ void loop() {
     /* Creation and sending mqtt messages */
     snprintf(msg_single, MSG_BUFFER_SINGLE_SIZE, "%6.2f", (float)eventGyro.gyro.z);
     client.publish(topic_rot_speed, msg_single);
+    Serial.println("nom du topic");
+    Serial.println(topic_rot_speed);
+    Serial.println("message msg_single");
+    Serial.println(msg_single);
   }
-  if (cfg.buttons) { 
-    /* Get buttons one and two states */
-    buttonOneState = digitalRead(buttonOnePin);
-    buttonTwoState = digitalRead(buttonTwoPin); 
-    /* Print plotable ide arduino data on serial port */
-    if (cfg.display) {
-      Serial.print("button1:");
-      Serial.print(buttonOneState);
-      Serial.print(", button2:");
-      Serial.print(buttonTwoState);
-      Serial.println("");
-    }
-    /* Creation and sending mqtt messages */
-    snprintf(msg_single, MSG_BUFFER_SINGLE_SIZE, "%s", buttonOneState == HIGH ? "false" : "true");
-    client.publish(topic_button1, msg_single);
-    snprintf(msg_single, MSG_BUFFER_SINGLE_SIZE, "%s", buttonTwoState == HIGH ? "false" : "true");
-    client.publish(topic_button2, msg_single);
-  }
+
   if (cfg.emg) {
-     /* Get emg value */
-    emg = analogRead(emgPin); 
+    /* Get emg value */
+    emg = analogRead(emgPin);
     /* Print plotable ide arduino data on serial port */
     if (cfg.display) {
       Serial.print("emg:");
@@ -209,50 +306,78 @@ void loop() {
     }
     /* Creation and sending mqtt messages */
     snprintf(msg_single, MSG_BUFFER_SINGLE_SIZE, "%6.2f", emg);
-    client.publish(topic_rot_speed, msg_single);
+    client.publish(topic_emg, msg_single);
   }
+
+  if (cfg.flex) {
+    /* Get flex value */
+    flexValue = analogRead(flexPin);
+    /* Creation and sending mqtt messages */
+    if ((flexValue < FLEX_LOW_LIMIT) && (flexState == OPEN)) {
+      flexState = CLOSE;
+      snprintf(msg_single, MSG_BUFFER_SINGLE_SIZE, "%d", flexState);
+      client.publish(topic_emg, msg_single);
+      if (cfg.display) {
+        Serial.print("Sending message on topic ");
+        Serial.println(topic_emg);
+      }
+    } else if ((flexValue > FLEX_HIGH_LIMIT) && (flexState == CLOSE)) {
+      flexState = OPEN;
+      snprintf(msg_single, MSG_BUFFER_SINGLE_SIZE, "%d", flexState);
+      client.publish(topic_emg, msg_single);
+      if (cfg.display) {
+        Serial.print("Sending message on topic ");
+        Serial.println(topic_emg);
+      }
+    }
+    /* Print plotable ide arduino data on serial port */
+    if (cfg.display) {
+      Serial.print("flex:");
+      Serial.print(flexValue);
+      Serial.print(" ----- ");
+      Serial.println((flexState == OPEN ? "OPEN" : "CLOSE"));
+    }
+  }
+
   delay(cfg.delay);
 }
 
 void setupWifi() {
-    delay(10);
+  delay(10);
+  if (cfg.master) {
     /* Start Wifi connection */
-    WiFi.begin(cfg.ssid);
-    Serial.print("Connecting to ");
-    Serial.println(cfg.ssid);
-    if ( strlen(cfg.password) == 0 ) {
-      WiFi.begin(cfg.ssid);
-    } else {
-      WiFi.begin(cfg.ssid, cfg.password);
-    }
-    
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("Connected to the WiFi network");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(cfg.ssid, cfg.password);
+    Serial.print("Hosting Wi-Fi hotspot : ");
+    Serial.print(cfg.ssid);
+    Serial.print(" with password ");
+    Serial.println(cfg.password);
+    delay(200);
     Serial.print("Local ESP32 IP: ");
     Serial.println(WiFi.localIP());
+  } else {
+    /* Connect to specified Wi-Fi network */
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(cfg.ssid, cfg.password);
+    Serial.print("Connecting to Wi-Fi ");
+    Serial.println(cfg.ssid);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial << '.';
+    }
+    Serial.print("Connected to Wi-Fi ");
+    Serial.println(cfg.ssid);
+    if (cfg.wifi_led) {
+      digitalWrite(cfg.led_wifi_pin, HIGH);
+    }
+  }
 }
 
-void reConnect() {
-    while (!client.connected()) {
-        Serial.print("Attempting MQTT connection...");
-        // Create a random client ID.  
-        String clientId = "ESP_IMU-";
-        clientId += String(random(0xffff), HEX);
-        // Attempt to connect. 
-        if (client.connect(clientId.c_str())) {
-            Serial.println("\nSuccess\n");
-            // Once connected, publish an announcement to the topic.
-            client.publish(cfg.topic_main, "ESP_IMU init");
-            // ... and resubscribe.
-            client.subscribe(cfg.topic_main);
-        } else {
-            Serial.print("failed, rc=");
-            Serial.print(client.state());
-            Serial.println("try again in 5 seconds");
-            delay(5000);
-        }
-    }
+float batteryLevel(int analogpin, float max_bat) {
+  int rawValue = analogRead(analogpin);
+  /* voltageLevel = ((Readed value) * bridge divider value * regulator output volatage ) + offset tuning */
+  float voltageLevel = ((rawValue / 4095.0) * 2.05 * 3.289) + 0.35 ;  // calculate voltage level
+  float percentage = (voltageLevel / max_bat) * 100;
+  Serial.println((String) " Voltage:" + voltageLevel + "V Percent: " + percentage + "%");
+  return percentage;
 }
